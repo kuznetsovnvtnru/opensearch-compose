98551285705548c9bff6eb94fbc7a97a48313104
Кузнецов Николай г. Рязань (СЭКР) <kuznetsovnv@tn.ru>
2023-08-24 15:30:11 +0300

    Fix yaml 3

--- opensearch-docker-entrypoint.sh
+++ opensearch-docker-entrypoint.sh
@@ -46,10 +46,7 @@ function setupSecurityPlugin {
             echo "Enabling OpenSearch Security Plugin"
             
             chmod +x /usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh
-            exec "/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/opensearch/config/opensearch-security/ \
-            -icl -nhnv -cacert /usr/share/opensearch/config/certificates/ca/ca.pem \
-            -cert /usr/share/opensearch/config/certificates/ca/admin.pem \
-            -key /usr/share/opensearch/config/certificates/ca/admin.key"
+            exec "/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/opensearch/config/opensearch-security/ -icl -nhnv -cacert /usr/share/opensearch/config/certificates/ca/ca.pem -cert /usr/share/opensearch/config/certificates/ca/admin.pem -key /usr/share/opensearch/config/certificates/ca/admin.key"
 
         fi
     else
